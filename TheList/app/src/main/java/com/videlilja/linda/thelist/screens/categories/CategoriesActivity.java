a6bf61f7c52c48219c751484d73bf6fe5451f5fe
Ellinor Heimer <barbellsparkles@gmail.com>
2018-01-23 12:47:17 +0100

    Hejsan
    Hejsan

--- TheList/app/src/main/java/com/videlilja/linda/thelist/screens/categories/CategoriesActivity.java
+++ TheList/app/src/main/java/com/videlilja/linda/thelist/screens/categories/CategoriesActivity.java
@@ -17,5 +17,7 @@ public class CategoriesActivity extends AppCompatActivity {
 
     public static void start(Context context){
         context.startActivity(new Intent(context,CategoriesActivity.class));
+
+        //Hejsan
     }
 }
