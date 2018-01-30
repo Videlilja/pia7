56bf65a846f3d08c545693ab04844faa8beac433
Videlilja <34338475+Videlilja@users.noreply.github.com>
2018-01-30 10:38:06 +0100

    fix splash activity to On Create

--- TheList/app/src/main/java/com/videlilja/linda/thelist/screens/categories/CategoriesActivity.java
+++ TheList/app/src/main/java/com/videlilja/linda/thelist/screens/categories/CategoriesActivity.java
@@ -4,9 +4,9 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -24,7 +24,10 @@ public class CategoriesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_categories);
 
         CategoriesViewModel viewModel = ViewModelProviders.of(this).get(CategoriesViewModel.class);
-
+        //Recyclerview
+        RecyclerView categoryList = findViewById(R.id.category_list);
+        categoryList.setLayoutManager(new LinearLayoutManager(this));
+        categoryList.setAdapter(new CategoriesAdapter());
         viewModel.getCategory().observe(this, new Observer<List<ListEntry>>() {
             @Override
             public void onChanged(@Nullable List<ListEntry> listEntries) {
@@ -34,13 +37,9 @@ public class CategoriesActivity extends AppCompatActivity {
 
     }
 
-    public void start(Context context){
-        context.startActivity(new Intent(context,CategoriesActivity.class));
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, CategoriesActivity.class));
 
-        //Recyclerview
-        RecyclerView categoryList = findViewById(R.id.category_list);
-        categoryList.setLayoutManager(new LinearLayoutManager(this));
-        categoryList.setAdapter(new CategoriesAdapter());
 
     }
 
