4df3f2d8433103beac7aaaf8696302be5cf668d2
Lena jehartmizander <lena.jehart.mizander@gmail.com>
2018-02-08 09:37:14 +0100

    provatlistviewmm

--- TheList/app/src/main/java/com/videlilja/linda/thelist/screens/categories/CategoriesActivity.java
+++ TheList/app/src/main/java/com/videlilja/linda/thelist/screens/categories/CategoriesActivity.java
@@ -1,5 +1,6 @@
 package com.videlilja.linda.thelist.screens.categories;
 
+import android.app.Activity;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
@@ -12,8 +13,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.videlilja.linda.thelist.R;
 import com.videlilja.linda.thelist.model.CategoriesEntry;
@@ -21,14 +25,50 @@ import com.videlilja.linda.thelist.model.CategoriesEntry;
 import java.util.List;
 
 public class CategoriesActivity extends AppCompatActivity {
+    ListView listView;
 
     private static final String TAG = "CategoriesActivity";
     private CategoriesViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        //Lena:
+        listView = ()findViewById(R.id.category_list);
+        String[] values = new String[]{"button1", "button2", "botton3"};
+        //Lena
+        ArrayAdapter<String> = adapter = new ArrayAdapter<String>(this,
+                android.R.layout.activity_list_item,android.R.id.text1,values);
+        listView.setAdapter();
+        listView.setOnClickListener(new AdapterView.OnItemClickListener()
+        {
+            //Lena:
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id){
+                if (position==0){
+                    Intent myintent = new Intent(view.getContext(),Activity1.class);
+                    startActivityForResult(myintent,0);
+
+                }
+
+                if (position==1){
+                    Intent myintent = new Intent(view.getContext(),Activity2.class);
+                    startActivityForResult(myintent,1);
+
+                }
+                if (position==2){
+                    Intent myintent = new Intent(view.getContext(),Activity3.class);
+                    startActivityForResult(myintent,2);
+
+                }
+
+
+
+            }
+            )};
 
         viewModel = ViewModelProviders.of(this).get(CategoriesViewModel.class);
 
@@ -88,3 +128,5 @@ public class CategoriesActivity extends AppCompatActivity {
 
 
 }
+
+
